9bf128897d9f7439a2f7dbfde19d039f85573716
madhurad <madhura.deshpande@cloudchainsoftware.com>
2019-06-09 16:57:56 +0400

    datetimepicker added for contract start date and contract end date

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $('#contract-startDate').datetimepicker({
+        timepicker: false,
+        format: 'd-m-Y'
+    });
+
+    $('#contract-endDate').datetimepicker({
+        timepicker: false,
+        format: 'd-m-Y'
+    });
+});
