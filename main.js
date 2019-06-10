8ad77da8eebdbf351a918ad7feab4bf00a2aaa9b
madhurad <madhura.deshpande@cloudchainsoftware.com>
2019-06-10 12:02:28 +0400

    Form Validation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+/* -- Contract Start Date and Contract End Date input fields made datepicker */
 $(function() {
 
     $('#contract-startDate').datetimepicker({
@@ -8,5 +10,23 @@ $(function() {
     $('#contract-endDate').datetimepicker({
         timepicker: false,
         format: 'd-m-Y'
+    });
+});
+/* -- ./..Contract Start Date and Contract End Date input fields made datepicker */
+
+$(function() {
+
+    $('#submitBtn').on('click', function() {
+        let fName = document.getElementById('firstName').value;
+        let lName = document.getElementById('lastName').value;
+        let contactNum = document.getElementById('contactNumber').value;
+        let contractStart = document.getElementById('contract-startDate').value;
+        let contractEnd = document.getElementById('contract-endDate').value;
+        let billingCycle = document.getElementById('billingCycle').value;
+
+        if((fName == "") || (lName == "") || (contactNum == "") || (contractStart == "") || 
+            (contractEnd == "") || (billingCycle == "")) {
+            alert('throw error message');
+        }
     });
 });
